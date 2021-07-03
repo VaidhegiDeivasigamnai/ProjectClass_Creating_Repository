42c339402de68049b6de7970ac1a29d2c0dbc786
VaidhegiDeivasigamnai <deivasigamani.vaidhegi@gmail.com>
2021-07-03 19:40:36 +1000

    A works

--- src/org/computer/DesktopModel.java
+++ src/org/computer/DesktopModel.java
@@ -17,11 +17,16 @@ public class DesktopModel implements Software, Hardware{
 		System.out.println("Latest Model"); 
 
 	}
+	public void laptopModel() {
+		System.out.println("good graphics");
+
+	}
 	public static void main(String[] args) {
 		DesktopModel d = new DesktopModel();
 		d.desktopModel();
 		d.hardwareresources();
 		d.softwareResources();
+		d.laptopModel();
 	}
 
 }
